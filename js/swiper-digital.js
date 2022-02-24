9cd36300fba9816d1134ba1b18ce245e13e86c4e
vrbr <lpvamaria@gmail.com>
2022-02-24 17:33:59 +0200

    правки

--- js/swiper-digital.js
+++ js/swiper-digital.js
@@ -1,12 +1,47 @@
+// "use strict";
+
+// const stickerSwiper = new Swiper(".swiper-choice", {
+//     slidesPerView: "auto",
+//     spaceBetween: 30,
+//     simulateTouch: true,
+//     // touchRatio: 1,
+//     // touchAngle: 45,
+//     grabCursor: true,
+//     keyboard: {
+//         enabled: true,
+//         onlyInViewport: true
+//     },
+//     navigation: {
+//         nextEl: ".choice-button-next",
+//         prevEl: ".choice-button-prev",
+//     },
+//     pagination: {
+//         el: ".choice-pagination",
+//         clickable: true,
+//         dynamicBullets: true,
+//     },
+//     breakpoints: {
+//         768: {
+//         spaceBetween: 25,
+//         },
+//         1200: {
+//         spaceBetween: 35
+//         }
+//     },
+// });
+
 "use strict";
 
 const stickerSwiper = new Swiper(".swiper-choice", {
     slidesPerView: "auto",
     spaceBetween: 30,
     simulateTouch: true,
-    // touchRatio: 1,
-    // touchAngle: 45,
+    touchRatio: 1,
+    touchAngle: 45,
     grabCursor: true,
+    hashNavigation: {
+        watchState: true
+    },
     keyboard: {
         enabled: true,
         onlyInViewport: true
@@ -22,10 +57,12 @@ const stickerSwiper = new Swiper(".swiper-choice", {
     },
     breakpoints: {
         768: {
-        spaceBetween: 25,
+        spaceBetween: 25
         },
         1200: {
         spaceBetween: 35
         }
     },
+    watchSlidesProgress: true,
+    watchSlidesvisibility: true
 });
